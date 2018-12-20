b39bfad947d1701d9e87abe395bd6e53ead82895
Lawrence <lcchou2@gmail.com>
2018-12-20 14:15:23 -0800

    basic reqs working, will add more

--- app.js
+++ app.js
@@ -1,20 +1,78 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
+  var displayfield = []
+
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
 
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+    
+    if (displayfield.length === 0) {
+      $('.list-display-field').text('')
+    }
+    
+    
+    
+    displayfield.push($('.text-entry').val())
+
+    localStorage.setItem($('.text-entry').val(), 0)
+
+
+
+    news = $('<div id="newline"></div>')
+    news.text($('.text-entry').val())
+
+    $('.list-display-field').append(news)
+
+    
+    
+ 
+
+    
+   
+
+
+    // localStorage.setItem($('.text-entry').val(), keyo);
+    
+    // var keyss = localStorage.keys()
+    // for (var i = 0; i < keyss.length; i ++) {
+    //   $('.list-display-field').text(keyss[i])
+    // }
+    
+
+  
+
+
 
   });
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    
+    localStorage.removeItem($('.text-entry').val());
+    displayfield = displayfield.filter(el => el !== $('.text-entry').val())
+    
+    $('.list-display-field').html('')
+
+    for (var i=0; i < displayfield.length; i ++) {
+      
+      if (displayfield[i] !== $('.text-entry').val()) {
+        news = $('<div id="newline"><img>></div>')
+        news.text(displayfield[i])
+
+        $('.list-display-field').append(news)
+      }
+
+      
+    }
+
+
+
   });
+
+  $('.btn-deleteall').on('click', function() {
+    $('.list-display-field').html('')
+    localStorage.clear()
+  })
 
 });
